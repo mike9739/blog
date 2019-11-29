de482e26a8fbd392f8789dd54058e53f80f0f062
mike9739 <migu_el_39@hotmail.com>
2019-11-29 11:49:32 -0600

    Configuramos el panel de administrador Creamos un super usuario Creamos la aplicacion portfolio Configuramos la aplicacion portfolio Creamos el modelo para la aplicacion portfolio

--- blog/settings.py
+++ blog/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'portfolio'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+##configurar donde se guardan los archivos 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,"media")
+
+
+
+
